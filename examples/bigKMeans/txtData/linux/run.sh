031a47591a08f550e6f3f2a5c0b23ec018522fd6
Fang Du <fangdu64@gmail.com>
2015-04-20 23:01:21 -0400

    added bigKmeans example script for linux

---
+++ examples/bigKMeans/txtData/linux/run.sh
@@ -0,0 +1,14 @@
+scriptPath=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+${scriptPath}/../../../config/bdt_path_linux.sh
+
+${bdtInstallDir}/bigKmeans \
+	--input-type text-mat \
+	--data ${bdtDatasetsDir}\txtMat\dnase_test.txt \
+	--nrow 92554 \
+	--ncol 45 \
+	--k 100 \
+	--out ${scriptPath}/out \
+	--thread-num 4 \
+	--dist-type Euclidean \
+	--max-iter 100 \
+	--min-expchg 0.0001
---
+++ examples/config/bdt_path_linux.sh
@@ -0,0 +1,2 @@
+bdtInstallDir="~/BDT-1.0.0/install"
+bdtDatasetsDir="~/BDT-1.0.0/bdtDatasets"
