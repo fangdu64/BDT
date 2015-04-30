c24ee55cc7a1c8af22a09ad1e46648e5dcd4dece
Fang Du <fangdu64@gmail.com>
2015-04-30 02:43:54 -0400

    added more options

--- bdtR/Bioconductor/bdt/R/bigKmeans.R
+++ bdtR/Bioconductor/bdt/R/bigKmeans.R
@@ -22,31 +22,61 @@
 #' @export
 #'
 bigKmeans <- function(bdt_home,
-                      input,
-                      row_cnt = NULL,
-                      col_cnt = NULL,
+                      data_input,
+                      data_nrow = NULL,
+                      data_ncol = NULL,
+                      data_col_names = NULL,
                       k = 100,
                       thread_num = 4,
                       dist_type = 'Euclidean',
                       max_iter = 100,
                       min_expchange = 0.0001,
+                      seeding_method = NULL,
+                      seeds_input = NULL,
+                      seeds_nrow = NULL,
+                      seeds_ncol = NULL,
+                      start_from = NULL,
                       out) {
     cmds = c(
         'py',
         paste0(bdt_home,"/bigKmeans"),
-        '--data-input', input,
+        '--data-input', data_input,
         '--k', as.character(k),
         '--out', out,
         '--thread-num', as.character(thread_num),
         '--dist-type', dist_type,
         '--max-iter', as.character(max_iter),
         '--min-expchg', as.character(min_expchange))
-    if (!is.null(row_cnt)) {
-        cmds <- append(cmds, c('--data-nrow', as.character(row_cnt)))
+    if (!is.null(data_nrow)) {
+        cmds <- append(cmds, c('--data-nrow', as.character(data_nrow)))
     }
 
-    if (!is.null(col_cnt)) {
-        cmds <- append(cmds, c('--data-ncol', as.character(col_cnt)))
+    if (!is.null(data_ncol)) {
+        cmds <- append(cmds, c('--data-ncol', as.character(data_ncol)))
+    }
+
+    if (!is.null(data_col_names)) {
+        cmds <- append(cmds, c('--data-col-names', paste0(data_col_names, collapse=",")))
+    }
+
+    if (!is.null(seeding_method)) {
+        cmds <- append(cmds, c('--seeding-method', seeding_method))
+    }
+
+    if (!is.null(seeds_nrow)) {
+        cmds <- append(cmds, c('--seeds-nrow', as.character(seeds_nrow)))
+    }
+
+    if (!is.null(seeds_ncol)) {
+        cmds <- append(cmds, c('--seeds-ncol', as.character(seeds_ncol)))
+    }
+
+    if (!is.null(seeds_input)) {
+        cmds <- append(cmds, c('--seeds-input', seeds_input))
+    }
+
+    if (!is.null(start_from)) {
+        cmds <- append(cmds, c('--start-from', start_from))
     }
 
     if (.Platform$OS.type == "windows") {
