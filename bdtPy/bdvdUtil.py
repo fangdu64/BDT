05239e7def91c37634e508d1f4e1caf38e1e3b8c
Fang Du <fangdu64@gmail.com>
2015-08-04 10:35:48 -0400

    added bdvd export related files

--- bdtPy/bdvdUtil.py
+++ bdtPy/bdvdUtil.py
@@ -176,3 +176,121 @@ def run_bdvd_ruv(
     proc = subprocess.call(node_cmd)
     gRunner.logp("end subtask \n")
     return (nodeDir,out_picke_file)
+
+bdvd_export_usage = '''
+bdvd-export
+'''
+
+def run_bdvd_ruv_export(
+    gRunner,
+    platform,
+    bdtHomeDir,
+    nodeName,
+    runDir,
+    dryRun,
+    removeBeforeRun,
+    rowIdxsPickle,
+    row_selector,
+    rowidx_from,
+    rowidx_to,
+    column_ids,
+    workercnt,
+    memory_size
+    ):
+
+    nodeDir = os.path.abspath("{0}/{1}".format(runDir, nodeName))
+
+    out_picke_file = os.path.abspath("{0}/{1}.pickle".format(nodeDir,nodeName))
+    if dryRun:
+        return out_picke_file
+
+    if removeBeforeRun and os.path.exists(nodeDir):
+        gRunner.logp("remove existing dir: {0}".format(nodeDir))
+        shutil.rmtree(nodeDir)
+    
+    nodeScriptDir = nodeDir + "-script"
+    if not os.path.exists(nodeScriptDir):
+        os.mkdir(nodeScriptDir)
+
+    #
+    # prepare design file
+    #
+    if pre_normalization is None:
+        CommonLibrarySize = 0
+    elif pre_normalization == "column-sum" and common_column_sum is None:
+        CommonLibrarySize = -1 # use median column-sum
+    else:
+        CommonLibrarySize = common_column_sum
+
+    if ruv_type == "ruvs" and ruv_rowwise_adjust is None:
+        RUVMode = iBS.RUVModeEnum.RUVModeRUVs
+    elif ruv_type == "ruvs" and ruv_rowwise_adjust == "unitary-length":
+        RUVMode = iBS.RUVModeEnum.RUVModeRUVsForVariation
+    elif ruv_type == "ruvg" and ruv_rowwise_adjust is None:
+        RUVMode = iBS.RUVModeEnum.RUVModeRUVg
+    elif ruv_type == "ruvg" and ruv_rowwise_adjust == "unitary-length":
+        RUVMode = iBS.RUVModeEnum.RUVModeRUVgForVariation
+
+    ControlFeaturePolicy = iBS.RUVControlFeaturePolicyEnum.RUVControlFeaturePolicyNone
+    ControlFeatureMaxCntLowBound = None
+    ControlFeatureMaxCntUpBound = None
+    CtrlQuantile = None
+    AllInQuantileFraction = None
+    if control_rows_method == "all":
+        ControlFeaturePolicy = iBS.RUVControlFeaturePolicyEnum.RUVControlFeaturePolicyNone
+    elif control_rows_method == "weak-signal":
+        ControlFeaturePolicy = iBS.RUVControlFeaturePolicyEnum.RUVControlFeaturePolicyMaxCntLow
+        ControlFeatureMaxCntLowBound = weak_signal_lb
+        ControlFeatureMaxCntUpBound = weak_signal_ub
+    elif control_rows_method == "lower-quantile":
+        ControlFeaturePolicy = iBS.RUVControlFeaturePolicyEnum.RUVControlFeaturePolicyAllInLowerQuantile
+        CtrlQuantile = lower_quantile_threshold
+        AllInQuantileFraction = all_in_quantile_fraction
+    elif control_rows_method == "specified-rows":
+        ControlFeaturePolicy = iBS.RUVControlFeaturePolicyEnum.RUVControlFeaturePolicyFeatureIdxList
+
+    MaxK = len(sampleGroups)
+
+    design_params=(sampleGroups,
+                   KnownFactors,
+                   CommonLibrarySize,
+                   RUVMode,
+                   ControlFeaturePolicy,
+                   ControlFeatureMaxCntLowBound,
+                   ControlFeatureMaxCntUpBound,
+                   CtrlQuantile,
+                   AllInQuantileFraction,
+                   MaxK,
+                   featureIdxFrom,
+                   featureIdxTo,
+                   permutation_cnt)
+    params_pickle_fn=os.path.abspath("{0}/design_params.pickle".format(nodeScriptDir))
+    iBSDefines.dumpPickle(design_params, params_pickle_fn)
+
+    design_file=os.path.abspath(bdtHomeDir+"/bdt/bdtPy/PipelineDesigns/bdvdRUVDesign.py")
+    shutil.copy(design_file,nodeScriptDir)
+
+    #
+    # Run node
+    #
+    design_fn=os.path.abspath(nodeScriptDir)+"/bdvdRUVDesign.py"
+    cmdpath=os.path.abspath("{0}/bdt/bdtCmds/bdvd-ruv".format(bdtHomeDir))
+    if platform == "Windows":
+        node_cmd = ["py", cmdpath]
+    else:
+        node_cmd = [cmdpath]
+    node_cmd.extend(["--node", nodeName,
+                "--num-threads", str(workercnt),
+                "--max-mem", str(memory_size),
+                "--out", nodeDir,
+                "--data-mat", datamatPickle])
+    
+    if ctrlRowPickle is not None:
+        node_cmd.extend(["--ctrl-rows", ctrlRowPickle])
+    
+    node_cmd.append(design_fn)
+
+    gRunner.logp("run subtask at: {0}".format(nodeDir))
+    proc = subprocess.call(node_cmd)
+    gRunner.logp("end subtask \n")
+    return (nodeDir,out_picke_file)
