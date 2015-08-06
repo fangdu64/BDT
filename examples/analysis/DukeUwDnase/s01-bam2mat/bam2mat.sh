724278f1a84b5c95427f770290eff36ac306deb7
Fang Du <fangdu64@gmail.com>
2015-08-06 09:06:31 -0400

    --calc-statistics

--- examples/analysis/DukeUwDnase/s01-bam2mat/bam2mat.sh
+++ examples/analysis/DukeUwDnase/s01-bam2mat/bam2mat.sh
@@ -8,4 +8,5 @@ ${bdtHome}/bigMat \
 	--chromosomes chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX \
 	--bin-width 100 \
 	--thread-num 40 \
+	--calc-statistics \
 	--out ${thisScriptPath}/out
