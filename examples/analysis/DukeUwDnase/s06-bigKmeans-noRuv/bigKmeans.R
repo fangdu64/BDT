0f9b519b5027a42c156524c15bee1773b2a4d42e
Fang Du <fangdu64@gmail.com>
2015-08-07 13:12:44 -0400

    added bigKmeans script for Duke Uw dnase analysis

---
+++ examples/analysis/DukeUwDnase/s06-bigKmeans-noRuv/bigKmeans.R
@@ -0,0 +1,15 @@
+rm(list=ls())
+library("bdt")
+
+thisScriptDir = getScriptDir()
+source(paste0(thisScriptDir, '/../../../config/bdt_path.R'))
+
+ret = bigKmeans(
+    bdt_home = bdtHome,
+    data_input = paste0("output@", thisScriptDir, '/../s04-export-noRuv/out'),
+    k = 100,
+    thread_num = 40,
+    dist_type = 'Correlation',
+    max_iter = 100,
+    min_expchange = 0.0001,
+    out = paste0(thisScriptDir,"/out"))
